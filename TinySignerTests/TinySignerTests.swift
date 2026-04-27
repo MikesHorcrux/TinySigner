045af9e0902f30b65725305ea906e6091da5faba
Mike <vanamburg.mv@gmail.com>
2026-04-27 09:25:51 -0500

    Initial Commit

---
+++ TinySignerTests/TinySignerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinySignerTests.swift
+//  TinySignerTests
+//
+//  Created by Mike  Van Amburg on 4/27/26.
+//
+
+import Testing
+@testable import TinySigner
+
+struct TinySignerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
